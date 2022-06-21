63f2f8e5719fb40fe6ed62246f829435ab8e8af6
P_7178 <francisco.edmundo@camara.leg.br>
2022-06-21 02:48:20 -0300

    SaveRelevanceFeedback: ajustes finais de integração

--- Microservices/save-relevance-feedback/app.py
+++ Microservices/save-relevance-feedback/app.py
@@ -1,71 +1,75 @@
-from flask import Flask, request, Response, jsonify
-from datetime import datetime
+import os
+import traceback
+
+from flask import Flask, request, Response
 import psycopg2
 import json
 
 VALID_CLASSES = ["r", "i", "vr", "d", "pr"] # relevante; irrelevante; muito relevante; duvida; pouco relevante
-INSERT_ENTRY = "INSERT INTO feedback(query, user_feedback, extra_results, date_created) VALUES ('{}', '{}', '{}', '{}');"
+INSERT_ENTRY = "INSERT INTO feedback(query, user_feedback, extra_results, date_created, user_id) VALUES ({}, {}, {}, CURRENT_TIMESTAMP, {});"
 
 app = Flask(__name__)
 
-connection = psycopg2.connect(host="ulyssesdb", database="admin",user="admin", password="admin", port=5432)
+db_connection = os.getenv('ULYSSES_DB_CONNECTION', default='host=ulyssesdb database=admin user=admin password=admin port=5432')
+connection = psycopg2.connect(db_connection)
 
 def isResultValid(result):
     try:
-        keys = list(result.keys())
-        if (len(keys) != 5):
-            return False
-
         score = float(result["score"])
         score_normalized = float(result["score_normalized"])
+        score_final = float(result["score_final"])
         classification = result["class"]
         code = result["id"]
         tipo = result["tipo"]
 
         if (classification not in VALID_CLASSES):
             return False
-    except:
+    except Exception as ex:
+        traceback.print_exc()
         return False
     return True
 
 
 def isValid(entry):
     try:
-        keys = list(entry.keys())
-        if (len(keys) != 3):
-            return False
-        
-        query = entry["query"]
-        results = entry["results"]
-        extra_results = entry["extra_results"]
+        # Parâmetros obrigatórios
+        query, results, extra_results = entry["query"], entry["results"], entry["extra_results"]
         if (type(results) != list):
             results = json.loads(results)
 
         for result in results:
             if (not isResultValid(result)):
                 return False
-    except: 
+    except Exception as ex:
+        traceback.print_exc()
         return False
     return True
 
 @app.route('/', methods=["POST"])
 def registerScores():
     data = request.json
-
-    if(isValid(data)):
+    if (isValid(data)):
         try:
             query = data["query"]
             results = data["results"]
             extra_results = data["extra_results"]
-            date = datetime.utcnow()
-            print(results, flush=True)
+            user_id = data.get("user_id")
+            json_results = json.dumps(results)
+            json_extra_results = json.dumps(extra_results) if extra_results else None
             with connection.cursor() as cursor:
-                cursor.execute( INSERT_ENTRY.format(query, json.dumps(results), json.dumps(extra_results), date) )
-                connection.commit()
-        except:
-            return Response(status=500) 
+                try:
+                    cursor.execute(INSERT_ENTRY.format(sql_quote(query), sql_quote(json_results), sql_quote(json_extra_results), sql_quote(user_id)))
+                    connection.commit()
+                except Exception as ex:
+                    connection.rollback()
+                    raise ex
+            return Response(status=201)
+        except Exception as ex:
+            traceback.print_exc()
+    return Response(status=500)
 
-    return Response(status=201)
+def sql_quote(text):
+    return "'" + text.replace("'", "''") + "'" if text else 'NULL'
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5001)
